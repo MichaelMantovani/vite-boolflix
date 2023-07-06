d8696c33c678b2297211c294fba1928625842ca5
MichaelMantovani <michmantovani@gmail.com>
2023-07-06 11:47:57 +0200

    Fix movie language icon

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,8 +1,18 @@
 <script>
+import { languageAvailable } from '../data/index'
 export default {
+  data() {
+    return { languageAvailable }
+  },
+
   methods: {
     getImagePath(img) {
-      return new URL(`../assets/img/${img}`, import.meta.url).href;
+      return new URL(`../assets/img/${img}.png`, import.meta.url).href;
+    }
+  },
+  computed: {
+    hasIcon() {
+      return this.languageAvailable.includes(this.movie.language)
     }
   },
   props: {
@@ -15,7 +25,10 @@ export default {
   <ul class="w-25 mx-auto my-5">
     <li>{{ movie.title }}</li>
     <li>{{ movie.originalTitle }}</li>
-    <li class="w-25"><img class="img-fluid" :src="getImagePath(movie.language)" src="../assets/img/" alt=""></li>
+    <li class="w-25">
+      <img v-if="hasIcon" class="img-fluid" :src="getImagePath(movie.language)" :alt="movie.language">
+      <span v-else>{{ movie.language }}</span>
+    </li>
     <li>{{ movie.vote }}</li>
   </ul>
 </template>
--- src/data/index.js
+++ src/data/index.js
@@ -1,5 +1,6 @@
 const baseUri = 'https://api.themoviedb.org/3'
 const apiKey = '85b4061722c282a492d446d05895f72a'
+const languageAvailable = ['en', 'it']
 
 
-export {baseUri, apiKey}
+export {baseUri, apiKey, languageAvailable}
