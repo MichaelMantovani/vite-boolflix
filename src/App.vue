37b5471c2bef3e9de018f3f5cd00c5a5c5e235da
MichaelMantovani <michmantovani@gmail.com>
2023-07-06 12:20:04 +0200

    Fix fetchFilteredProductions

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,13 @@ export default {
     return store
   },
   methods: {
-    fetchFilteredProductions(searchTerm) {
-      axios.get(`${baseUri}/search/movie?api_key=${apiKey}&query=${searchTerm}`)
+    fetchFilteredProductions(searchTerm, endpoint, target) {
+      axios.get(`${baseUri}/${endpoint}?api_key=${apiKey}&query=${searchTerm}`)
         .then(res => {
-          const movies = res.data.results
-          store.movies = movies.map((movie) => {
-            const { title, original_title, original_language, vote_average } = movie
-            return { title, originalTitle: original_title, language: original_language, vote: Math.ceil(vote_average) }
+          const productions = res.data.results
+          store[target] = productions.map((production) => {
+            const { title, original_title, original_language, vote_average } = production
+            return { title: title || name, originalTitle: original_title || original_name, language: original_language, vote: Math.ceil(vote_average) }
           })
         })
         .catch(err => console.error(err))
